03d748f9fda74ce6b93772f2ef69770075a5ecd9
Vedant Neve <148627417+0bVdnt@users.noreply.github.com>
2025-01-26 21:55:14 +0530

    Create run.sh

---
+++ learning-basics/implementing-simple-kernel/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+# Path to clang and compiler flags
+CC=/usr/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+#-Wl, means passing options to the linker instead of the C compiler. clang command does C compilation and executes the linker internally.
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
